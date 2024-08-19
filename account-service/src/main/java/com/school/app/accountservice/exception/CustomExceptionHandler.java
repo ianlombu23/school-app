7d88226ffaacdf59f4ebc9620bd3749e52f22766
Ian <ianlombu05@gmail.com>
2024-08-19 15:58:44 +0700

    Add log for exception handler CustomExceptionHandler

--- account-service/src/main/java/com/school/app/accountservice/exception/CustomExceptionHandler.java
+++ account-service/src/main/java/com/school/app/accountservice/exception/CustomExceptionHandler.java
@@ -1,15 +1,18 @@
 package com.school.app.accountservice.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
+@Slf4j
 public class CustomExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ErrorResponse> handleCustomException(CustomException ex, WebRequest request) {
+        log.info("customException: ", ex);
         ErrorResponse errorResponse = new ErrorResponse(ex.getErrorCode(), ex.getMessage());
         return new ResponseEntity<>(errorResponse, ex.getHttpStatus());
     }
